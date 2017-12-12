ff5fb13d771790fb2bb85a53144c34171dc01827
Tyler Quayle <taquayle@uw.edu>
2017-12-11 16:03:40 -0800

    Quick removed filler

--- Screens/Recorder/Weight/WeightHome.js
+++ Screens/Recorder/Weight/WeightHome.js
@@ -12,19 +12,6 @@ import { Button, SideMenu, List, ListItem, Icon, Header, Divider, FormInput, For
 import {  VictoryChart, VictoryCandlestick } from "victory-native";
 
 
-// Filler data so we can do some layout work
-const candleData = [
-  {x: 1, open: 9, close: 30, high: 56, low: 7},
-  {x: 2, open: 80, close: 40, high: 120, low: 10},
-  {x: 3, open: 50, close: 80, high: 90, low: 20},
-  {x: 4, open: 70, close: 22, high: 70, low: 5},
-  {x: 5, open: 20, close: 35, high: 50, low: 10},
-  {x: 6, open: 35, close: 30, high: 40, low: 3},
-  {x: 7, open: 30, close: 90, high: 95, low: 30},
-  {x: 8, open: 80, close: 81, high: 83, low: 75}
-];
-
-
 export class WeightHomeScreen extends React.Component{
   componentWillMount(){
     console.log("Current Screen: " + this.props.navigation.state.key)
@@ -39,23 +26,21 @@ export class WeightHomeScreen extends React.Component{
 
 
   submitWeight(){
-    if(this.state.addWeight == "")
+    if(this.state.addWeight == "") // Check if something has been entered
       return;
     var tempList = this.state.weightList.slice();
     var tempWeight = this.state.addWeight
     var tempDiff = parseInt(this.state.addWeight) - this.state.runningWeight
+
     if(this.state.runningWeight == 0)
       tempDiff = 0
     var today = new Date()
     var newDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
     tempList.push({'date': newDate, 'weight': this.state.addWeight, 'diff': tempDiff})
 
-    if(this.state.runningWeight == 0){
-      this.state.runningWeight = this.state.addWeight
-    }
     console.log(tempList)
     this.setState({
-      runningWeight: tempWeight,
+      runningWeight: tempWeight, // 'last' weight entered
       addWeight: "",
       weightList: tempList,
 
